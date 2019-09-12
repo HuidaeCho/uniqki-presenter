00cf829c71cf9670d44de10b3159f067a3632c71
Huidae Cho <grass4u@gmail.com>
2019-09-12 01:14:28 -0400

    Rename top-menu to read-config and add more settings

--- read.js
+++ read.js
@@ -5,23 +5,42 @@ loadCSS('u.tpl/screen.css', 'screen');
 loadCSS('u.tpl/read.css', 'screen');
 loadCSS('u.tpl/print.css', 'print');
 
-ajaxRequest('top-menu.html', null, function(xhr){
-	let view = ajaxResponseXML(xhr).getElementById('view');
+ajaxRequest('read-config.html', null, function(xhr){
+	let xml = ajaxResponseXML(xhr);
+	let view = xml.getElementById('view');
 	if(!view) return;
-	let items = [...view.getElementsByTagName('a')];
-	if(!items.length) return;
-	let menu = document.createElement('div');
-	menu.id = 'top-menu';
-	items.forEach(function(item){
-		if(item.href.replace(/\.html(#|$)/, '$1') == window.location.href.replace(/\.html(#|$)/, '$1'))
-			item.classList.add('active');
-		item.onclick = function(){
-			items.forEach(function(i){
-				i.classList.remove('active');
-			});
-			this.classList.add('active');
-		};
-		menu.appendChild(item);
-	});
-	document.getElementsByTagName('header')[0].appendChild(menu);
+	let config;
+	if((config = xml.getElementById('site-title-config'))){
+		let items = [...config.getElementsByTagName('li')];
+		if(!items.length) return;
+		items.forEach(function(item){
+			let text = item.firstChild;
+			if(!text || text.nodeName.toLowerCase() != '#text') return;
+			let value = text.textContent.split(/:/);
+			if(value[0] == 'mobile' && window.matchMedia('(max-width:360px)').matches)
+				document.getElementById('site-title').style.display = value[1];
+		});
+	}
+	if((config = xml.getElementById('top-menu-config'))){
+		let items = [...config.getElementsByTagName('li')];
+		if(!items.length) return;
+		let menu = document.createElement('div');
+		menu.id = 'top-menu';
+		let menuItems = [];
+		items.forEach(function(item){
+			let a = item.firstChild;
+			if(!a || a.nodeName.toLowerCase() != 'a') return;
+			if(a.href.replace(/\.html(#|$)/, '$1') == window.location.href.replace(/\.html(#|$)/, '$1'))
+				a.classList.add('active');
+			a.onclick = function(){
+				menuItems.forEach(function(i){
+					i.classList.remove('active');
+				});
+				this.classList.add('active');
+			};
+			menu.appendChild(a);
+			menuItems.push(a);
+		});
+		document.getElementsByTagName('header')[0].appendChild(menu);
+	}
 });
